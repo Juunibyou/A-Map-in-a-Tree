c33dc8899d2de59a91c1b7093189d684c386ddcd
Juunibyou <danijunn@gmail.com>
2025-11-12 13:54:40 -0600

    setup

--- app/src/main/java/org/example/SplayTree.java
+++ app/src/main/java/org/example/SplayTree.java
@@ -1,5 +1,36 @@
 package org.example;
 
 public class SplayTree<T extends Comparable<T>> {
-    
+    private class Node{
+        T data;
+        Node left, right;
+
+        Node(T data){
+            this.data = data;
+        }
+    }
+
+    private Node rotateRight(Node x){
+
+    }
+
+    private Node rotateLeft(Node x){
+
+    }
+
+    private Node splay(Node root, int key){
+
+    }
+
+    public void insert(T key){
+
+    }
+
+    public T find(T key){
+
+    }
+
+    public void delete(T key){
+        
+    }
 }
