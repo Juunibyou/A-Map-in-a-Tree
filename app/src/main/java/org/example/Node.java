b99a88d3e15a3156b1265a5e75719a5c82ffae8c
Juunibyou <danijunn@gmail.com>
2025-11-13 09:31:42 -0600

    update

---
+++ app/src/main/java/org/example/Node.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Node {
+    public String data;
+    public Node left, right;
+
+    public Node(String data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
--- app/src/main/java/org/example/SplayTree.java
+++ app/src/main/java/org/example/SplayTree.java
@@ -1,24 +1,51 @@
 package org.example;
 
-public class SplayTree<T extends Comparable<T>> {
-    private class Node{
-        T data;
-        Node left, right;
-
-        Node(T data){
-            this.data = data;
-        }
-    }
+public class SplayTree {
 
     private Node rotateRight(Node x){
-
+        Node y = x.left;
+        x.left = y.right;
+        y.right = x;
+        return y;
     }
 
     private Node rotateLeft(Node x){
-
+        Node y = x.right;
+        x.right = y.left;
+        y.left = x;
+        return y;
     }
 
-    private Node splay(Node root, int key){
+    private Node splay(Node root, String key){
+        if (root == null || root.data.compareTo(key) == 0){return root;}
+
+        if (key.compareTo(root.data) < 0){
+            if(root.left == null) {return root;}
+
+            //left-left
+            if (key.compareTo(root.left.data) < 0){
+                root.left.left = splay(root.left.left, key);
+                root = rotateRight(root);
+            }
+
+            //left-right
+            else if (key.compareTo(root.left.data) > 0){
+                root.left.right = splay(root.left.right, key);
+                if (root.left.right != null){
+                    root.left = rotateLeft(root.left);
+                }
+            }
+
+            return  (root.left == null) ? root : rotateRight(root);
+        }
+
+        else{
+            if(root.right == null){return root;}
+
+            //right-left
+
+            //right-right
+        }
 
     }
 
@@ -31,6 +58,6 @@ public class SplayTree<T extends Comparable<T>> {
     }
 
     public void delete(T key){
-        
+
     }
 }
